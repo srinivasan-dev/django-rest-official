256fa870053a82a26c25bd6d296cc8731c9fca04
srinivasan <srini@it-architecturegroup.com>
2020-04-07 22:35:36 +0530

    first commit

---
+++ tutorial/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/srinivasank/srini-workspace/python-workspace/django-workspace/rest-django/tutorial/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
